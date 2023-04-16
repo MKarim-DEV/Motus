89ff749dc90b077674c965760212fa4850ab420f
Kweezzy <karimmouellef@gmail.com>
2023-04-16 19:47:35 +0200

    fonction pour choisir un mot au hasard et l'afficher dans le tableau HTML

--- js/index.js
+++ js/index.js
@@ -1,15 +1,33 @@
 "use strict";
 
-let hiddenWordList=["ACADEMIE","ALLIANCE","BENEFICE","CALCAIRE","CHENILLE","CROISADE","DIALOGUE","ECUREUIL","ETRANGES","FLAMBEAU","FREQUENT","GENERALE","HARMONIE","ILLUSION","LABRADOR","MARECAGE","MINUTEUR","NATATION","OMNIVORE","PARASITE","PRECHEUR","PRIVATIF","REPTILES","RICHESSE","SALAIRES","STEROIDE","TENAILLE","TRIBUNAL","VAGABOND","VOLAILLE"];
+const HIDDEN_WORD_LIST=["ACADEMIE","ALLIANCE","BENEFICE","CALCAIRE","CHENILLE","CROISADE","DIALOGUE","ECUREUIL","ETRANGES","FLAMBEAU","FREQUENT","GENERALE","HARMONIE","ILLUSION","LABRADOR","MARECAGE","MINUTEUR","NATATION","OMNIVORE","PARASITE","PRECHEUR","PRIVATIF","REPTILES","RICHESSE","SALAIRES","STEROIDE","TENAILLE","TRIBUNAL","VAGABOND","VOLAILLE"];
 //30 mots cachés.
-function hiddenWordChoice(array){
+function hidden_word_choice(array){
     let randomChoice = Math.random()*array.length|0;
     let randomWord = array[randomChoice];
     return randomWord;
 }
 
-let randomWord = hiddenWordChoice(hiddenWordList);
+let randomWord = hidden_word_choice(HIDDEN_WORD_LIST);
 console.log(randomWord);
+console.log(randomWord[0]);
+console.log(randomWord[1]);
+console.log(randomWord[7]);
+  
+function display_in_table(word) {
+    const table = document.querySelector("table");
+    const letters = word.split('');
+  
+  for (let row = 0; row < 10; row++) {
+    for (let col = 0; col < 8; col++) {
+      const td = table.rows[row].cells[col];
+      td.innerHTML = letters[col];
+    }
+  }
+}
+display_in_table(randomWord);
+  
 
-let remplissageTableau = document.querySelector('tr');
-remplissageTableau.innerHTML = "randomWord";
+
+
+ 
